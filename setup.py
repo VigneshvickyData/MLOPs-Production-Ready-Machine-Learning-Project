9599f631fca88ac8b68a9e2818253ea680cf018f
VigneshvickyData <candycrushvicky7@gmail.com>
2024-09-30 18:53:47 +0530

    utils added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="vicky",
+    author_email="vigneshvicky170205@gmail.com",
+    packages=find_packages()
+)
